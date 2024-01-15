fda9717ca50d05927d660411f23481c85a4ccdab
Daviwizzy <davidakintunde433@gmail.com>
2024-01-15 20:13:54 +0100

    add all files to folder

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,1 +1,38 @@
 
+// SearchBar.js
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleSearch = () => {
+    // Implement your search logic here, using this.state.searchTerm
+    console.log('Search term:', this.state.searchTerm);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.state.searchTerm}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleSearch}>Search</button>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
